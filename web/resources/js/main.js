dcd1a8d8960d7bab084a4f75ce3db867bd94166a
evanschris <evanschris@me.com>
2014-06-09 16:19:13 +0100

    testing gif

--- web/resources/js/main.js
+++ web/resources/js/main.js
@@ -1,1 +1,8 @@
-
+$(".project-image").hover(function() {
+	var pLeft = $(".sequence").position();
+	pLeft = pLeft.left;
+	var frames = $(".sequence").data("frames");
+	var total = frames * 216
+	
+	$(".sequence").css( "left", (pLeft - 216) );
+});
