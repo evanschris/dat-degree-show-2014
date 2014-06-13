bfaee0504b008e5477f9c572414498cae0642a9b
evanschris <evanschris@me.com>
2014-06-13 09:54:05 +0100

    Created gif in 'giftest'
    using the structure found in giftest.html then using the js in gif.js

--- web/resources/js/main.js
+++ web/resources/js/main.js
@@ -1,14 +1,4 @@
 
-$(".project-image").hover(function() {
-	var pLeft = $(".sequence").position();
-	pLeft = pLeft.left;
-	var frames = $(".sequence").data("frames");
-	var total = frames * 216
-	for (var i = 0; i < total; i++) {
-		$(".sequence").css( "left", (pLeft - 216) );
-	}
-});
-
 $(document).ready(function(){
 
 	if($("#login").length > 0){
